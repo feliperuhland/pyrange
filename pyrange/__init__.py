6df7397754efa396ba11e5d416e2962e4eb2a169
Felipe Ruhland <felipe.ruhland@gmail.com>
2017-03-23 20:12:42 -0300

    RangeClass with cast

--- pyrange/__init__.py
+++ pyrange/__init__.py
@@ -2,15 +2,22 @@
 
 
 class BaseRange(object):
+    _cast = None
+
     def __call__(self, value):
         self.value = value
         self._attr = [
             (k, v)
             for k, v in self.__class__.__dict__.items()
-            if not k.startswith('__')]
+            if not k.startswith('_')]
 
+        value = self.value
+        cast = self.__class__._cast
         for k, v in self._attr:
-            res = v(self.value, k)
+            if cast:
+                value = cast(value)
+
+            res = v(value, k)
             if res:
                 return res
 
--- tests/test_range.py
+++ tests/test_range.py
@@ -10,9 +10,20 @@ class RangeExample(pyrange.BaseRange):
     false = pyrange.Field(lambda x: x % 2 != 0)
 
 
+class RangeIntExample(pyrange.BaseRange):
+    _cast = int
+    true = pyrange.Field(lambda x: x % 2 == 0)
+    false = pyrange.Field(lambda x: x % 2 != 0)
+
+
 @pytest.fixture
 def range_obj():
     return RangeExample()
+
+
+@pytest.fixture
+def range_int_obj():
+    return RangeIntExample()
 
 
 def test_simple_range(range_obj):
@@ -21,3 +32,10 @@ def test_simple_range(range_obj):
     assert range_obj(2) == 'true'
     assert range_obj(3) == 'false'
     assert range_obj(4) == 'true'
+
+
+def test_simple_cast(range_int_obj):
+    assert range_int_obj(0) == 'true'
+    assert range_int_obj('1') == 'false'
+    assert range_int_obj('2') == 'true'
+    assert range_int_obj(3) == 'false'
