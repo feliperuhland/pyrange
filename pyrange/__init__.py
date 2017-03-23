81f7fb3684abaabb6b7b9bd2e366158e4130a068
Felipe Ruhland <felipe.ruhland@gmail.com>
2017-03-23 20:38:06 -0300

    Operator field

--- pyrange/__init__.py
+++ pyrange/__init__.py
@@ -1,5 +1,18 @@
 # coding: utf-8
 
+import operator
+import re
+
+
+pattern = r'([<>=]+)?(\d+)'
+op = {
+    '<': operator.lt,
+    '<=': operator.le,
+    '>': operator.gt,
+    '>=': operator.ge,
+    '=': operator.eq,
+}
+
 
 class BaseRange(object):
     _cast = None
@@ -28,3 +41,14 @@ class Field(object):
 
     def __call__(self, value, field):
         return field if self.function(value) else None
+
+
+class RangeField(object):
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, value, field):
+        groups = re.search(pattern, self.function).groups()
+        operator_function = op[groups[0]]
+        num = int(groups[1])
+        return field if operator_function(int(value), num) else None
--- tests/test_range.py
+++ tests/test_range.py
@@ -16,6 +16,12 @@ class RangeIntExample(pyrange.BaseRange):
     false = pyrange.Field(lambda x: x % 2 != 0)
 
 
+class RangeFieldTenExample(pyrange.BaseRange):
+    less = pyrange.RangeField('<10')
+    equal = pyrange.RangeField('=10')
+    more = pyrange.RangeField('>10')
+
+
 @pytest.fixture
 def range_obj():
     return RangeExample()
@@ -24,6 +30,11 @@ def range_obj():
 @pytest.fixture
 def range_int_obj():
     return RangeIntExample()
+
+
+@pytest.fixture
+def range_ten():
+    return RangeFieldTenExample()
 
 
 def test_simple_range(range_obj):
@@ -39,3 +50,11 @@ def test_simple_cast(range_int_obj):
     assert range_int_obj('1') == 'false'
     assert range_int_obj('2') == 'true'
     assert range_int_obj(3) == 'false'
+
+
+def test_range_field_ten(range_ten):
+    assert range_ten(1) == 'less'
+    assert range_ten(9) == 'less'
+    assert range_ten(10) == 'equal'
+    assert range_ten(11) == 'more'
+    assert range_ten(1100) == 'more'
