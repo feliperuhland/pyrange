6b5c196022ced98c04d09b1d097b8cd9d318bfe7
Felipe Ruhland <felipe.ruhland@gmail.com>
2017-03-27 23:32:20 -0300

    Pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='pyrange',
+    packages=['pyrange'],
+    version='0.0.1',
+    description='A simple lib for define a range',
+    author='Felipe Ruhland',
+    author_email='felipe.ruhland@gmail.com',
+    url='https://github.com/feliperuhland/pyrange',
+    download_url='https://github.com/feliperuhland/pyrange/archive/0.0.1.tar.gz',
+    keywords=['range', 'search', 'class'],
+    classifiers=[])
